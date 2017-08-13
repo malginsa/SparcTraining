741e655a60bf624429075b552bd3ccf38e4c4117
malginsa <malginsa@gmail.com>
2017-08-13 19:33:18 +0300

    Ex_1_RDD_Intro added

--- src/main/scala/demo/Ex_1_RDD_Intro.scala
+++ src/main/scala/demo/Ex_1_RDD_Intro.scala
@@ -6,6 +6,7 @@ object Ex_1_RDD_Intro {
 
   def main(args: Array[String]): Unit = {
 
+    val workingFolder = "c:/Users/Sergei_Malgin/dev/SparcTraining/out/"
     System.setProperty("hadoop.home.dir", "d:\\hadoop_home_dir");
 
     val spark = SparkSession.builder
@@ -13,12 +14,81 @@ object Ex_1_RDD_Intro {
       .appName("RDD_Intro")
       .getOrCreate()
 
-    spark.sparkContext.setLogLevel("ERROR")
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
 
     val ds = spark.range(1000)
-    println(ds.count)
-    println(ds.rdd.count)
+    println(ds.count) // 1000
+    println(ds.rdd.count) //1000
+
+    val array = 1 to 10 toArray
+    val ints = sc.parallelize(array.reverse, 3) // RDD
+
+    // storing RDD as a "file"
+//    ints.saveAsTextFile("c:/Users/Sergei_Malgin/dev/SparcTraining/out/ints")
+
+    // reading RDD
+    val cachedInts = sc.textFile(workingFolder + "ints")
+        .map(x => x.toInt)
+      // кэширование - фиксация приготовленного RDD и файла
+      // в виде готового набора данных в памяти
+        .cache()
+
+    val squares = cachedInts.map(x=> x*x) // это RDD
+    // пока не сделали collect, все операции трансформации выполняются на стороне worker'ов
+    squares.collect().foreach(println)
+
+    val even = squares.filter(x => x%2 == 0) // это тоже RDD
+    even.collect().foreach(println)
+
+    even.setName("Even numbers")
+    println("Name is " + even.name + "id is " + even.id)
+    println(even.toDebugString) // инфа по RDD - сколько занимает в памяти, на диске...
+
+    println("Total multiplication is " + even.reduce((a,b) => a*b))
+
+    // summary of all array's elements
+    val array1 = 1 to 5 toArray
+    val summ = sc.parallelize(array1, 3).cache().reduce((a, b) => a+b)
+    println(summ + "\n")
+
+    // трансформация массива чисел в массив пар
+    val groups = cachedInts.map(x => if(x % 2 == 0) {
+      (0,x)
+    } else {
+      (1,x)
+    })
+
+    //
+    println(groups.groupByKey.toDebugString) // ShuffledRDD - произошло перемешивание
+    groups.groupByKey.collect.foreach(println)
+    println(groups.countByKey) // частотный анализ
+
+    println("\ncount " + groups.count)
+    println("first " + groups.first)
+    println("the first two ")
+    groups.take(2).foreach(println)
+    println("the first three ordered")
+    groups.takeOrdered(3).foreach(println)
+
+    val codeRows = sc.parallelize(Seq(("Ivan", 250), ("Elena", -15), ("Paetja", 50), ("Elena", 290)))
+    println("\nFake statistic of code writing performance")
+    codeRows.reduceByKey((a,b) => a+b).collect.foreach(println)
+
+    println("groupping by name ")
+    codeRows.groupByKey.collect.foreach(println)
+
+//    val programmersProfiles
+//    programmersProfiles = sc.parallelize(Seq(("Ivan", "Java"), ("Elena", "Scala"), ("Paetja", "Scala")))
+//    programmersProfiles.saveAsSequenceFile(workingFolder + "programmersProfiles")
+    println("Statistics of languages")
+    sc.sequenceFile(workingFolder + "programmersProfiles",
+        classOf[org.apache.hadoop.io.Text], // ключ был типа текст(хадуповский)
+        classOf[org.apache.hadoop.io.Text]) // и значение
+      .map{ case (x,y) => (x.toString, y.toString)} // pattern matching: преобр. в строки scal'ы
+      .join(codeRows)
+      .collect
+      .foreach(println)
 
   }
-
 }
